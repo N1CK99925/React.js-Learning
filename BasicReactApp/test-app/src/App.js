4d564bc2c0b60eb5ff8d6587be938a01f188da90
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2025-07-11 22:31:09 +0530

    some shit

--- BasicReactApp/test-app/src/App.js
+++ BasicReactApp/test-app/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+ 
 
+//Functional Component - write the first word captial 
+function MyButton (){
+  return <>
+  
+}
+const Search = () => <input placeholder="search sm shit"></input>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> 
+  </>
   );
 }
 
