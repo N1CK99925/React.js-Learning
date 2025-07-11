73891ed676b1a8c7cfb4c38c51458e4ed59b2eb9
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2025-07-11 23:19:39 +0530

    Logic in JSX

--- BasicReactApp/test-app/src/App.js
+++ BasicReactApp/test-app/src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
  
 
 //Functional Component - write the first word captial 
-const  Header = () => {
-  return (
-    <>
-    <div style={{backgroundColor:'grey',display:'flex',flex:'1 0 auto'}}>
-      <h1 className='h1'>Shopify App</h1>
-      {/* in react you write ClassName instead of class */}
-      <input placeholder='Search' />
-      <ul style={{display:'flex'}}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-    </>
-  )
-}
+let count = 0;
+
 
 function App() {
+
+  const onIncrementClick = () => {
+    count = count + 1
+    console.log({count})
+  }
+  const onSearchChange = (event) => {
+    console.log(event.target.value)
+  }
   return (
    <> 
-   <Header />
+  <button onClick={onIncrementClick}>Increment</button>
+  <input onChange={onSearchChange} placeholder='Search' />
   </>
   );
 }
